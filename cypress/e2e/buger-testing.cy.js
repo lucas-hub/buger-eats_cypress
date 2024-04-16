3a5d11a5e08137b1d1e6a6a33ecb86233fce1f61
lucas-hub <lucasbri444@gmail.com>
2024-04-15 22:46:27 -0300

    initial setup

---
+++ cypress/e2e/buger-testing.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+
+describe('Buger GUI Testing', ()=> {
+    beforeEach(()=> {
+        cy.visit('https://buger-eats-qa.vercel.app/')
+    })
+
+    it('Successfully', ()=> {
+        
+    })
+})
